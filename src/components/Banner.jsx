c5dab47a30179cf84f1bc3955b034df2017099d2
manu-2509 <maneshs111@gmail.com>
2022-07-03 22:43:38 +0530

    context

--- src/components/Banner.jsx
+++
@@ -1,8 +0,0 @@
-import "../styles.css"
-export const Banner=() =>{
-    return (
-        <div>
-            <img className="banner" src="https://d2407na1z3fc0t.cloudfront.net/Slider/banner_62a882919ddbf" alt="banner" />
-        </div>
-    )
-}
