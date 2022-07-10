32d05e35986df6fb642b9e42eaa596149fe19de2
manu-2509 <maneshs111@gmail.com>
2022-07-10 12:21:15 +0530

    adv

--- src/components/Adv.jsx
+++ src/components/Adv.jsx
@@ -2,6 +2,7 @@ import "../styles.css"
 export const Adv=()=>{
     return (
         <div>
+            <h1>eafsg</h1>
             <img className="adv-img" src="https://d2407na1z3fc0t.cloudfront.net/homepageStaticBanner/homepageStaticBanner_62a34b8cba7db" alt="adv" />
         </div>
         )
